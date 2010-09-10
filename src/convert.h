a58c520340ed9fb677b28576928c70d0c926fca1
nick <goodfella005@gmail.com>
2010-09-10 03:07:23 -0700

    Initial workings of the convert functions that actually check the validity of the optarg.

--- src/convert.h
+++ src/convert.h
@@ -1,12 +1,135 @@
 #ifndef __CONVERT_H__
 #define __CONVERT_H__
 
-#include <cassert>
+#include <errno.h>
 #include <cstdlib>
+#include <limits>
+#include <cassert>
 #include <string>
+
 
 namespace libgetopt
 {
+    template< class Type,
+	      class Func_Ret,
+	      class Func_Type,
+	      Func_Type Func,
+	      int Errno >
+    struct converter_info_base
+    {
+	    typedef Func_Ret func_return_t;
+	    static Func_Type func;
+	    static Type overflow_val;
+	    static Type underflow_val;
+	    static int range_errno;
+    };
+
+    template< class Type,
+	      class Func_Ret,
+	      class Func_Type,
+	      Func_Type Func,
+	      int Errno >
+    Func_Type converter_info_base<Type, Func_Ret, Func_Type, Func, Errno>::func = Func;
+
+    template< class Type,
+	      class Func_Ret,
+	      class Func_Type,
+	      Func_Type Func,
+	      int Errno >
+    Type converter_info_base<Type, Func_Ret, Func_Type, Func, Errno>::overflow_val =
+	std::numeric_limits<Type>::max();
+
+    template< class Type,
+	      class Func_Ret,
+	      class Func_Type,
+	      Func_Type Func,
+	      int Errno >
+    Type converter_info_base<Type, Func_Ret, Func_Type, Func, Errno>::underflow_val =
+	std::numeric_limits<Type>::min();
+
+
+    template< class Type,
+	      class Func_Ret,
+	      class Func_Type,
+	      Func_Type Func,
+	      int Errno >
+    int converter_info_base<Type, Func_Ret, Func_Type, Func, Errno>::range_errno = Errno;
+
+
+
+    template< class Type >
+    struct converter_info;
+
+    template<>
+    struct converter_info<int>: public converter_info_base<int,
+							   long,
+							   long (*)(char const * const, char**, int),
+							   &strtol,
+							   ERANGE>
+    {
+	    static int base;
+    };
+
+    int converter_info<int>::base = 10;
+
+
+    template<>
+    struct converter_info<long>: public converter_info_base<long,
+							    long,
+							    long (*)(char const * const, char**, int),
+							    &strtol,
+							    ERANGE>
+
+    {
+	    static int base;
+    };
+
+    int converter_info<long>::base = 10;
+
+
+    template<class Type>
+    bool integral_convert(char const * const num_str, std::string& err_str,
+			  Type* val)
+    {
+	assert( val != NULL );
+
+	char* bad_chrs = NULL;
+
+	errno = 0;
+
+	typename converter_info<Type>::func_return_t ret;
+
+	ret = converter_info<Type>::func(num_str,
+					 &bad_chrs,
+					 converter_info<Type>::base);
+
+	if( errno == converter_info<Type>::range_errno )
+	{
+	    assert(ret >= converter_info<Type>::overflow_val ||
+		   ret <= converter_info<Type>::underflow_val );
+
+	    if( ret >= converter_info<Type>::overflow_val )
+	    {
+		err_str = "arg is too large";
+		return false;
+	    }
+	    else if( ret <= converter_info<Type>::underflow_val )
+	    {
+		err_str = "arg is too small";
+		return false;
+	    }
+	}
+	else if( *bad_chrs != '\0' )
+	{
+	    err_str = "arg does not represent a numeric type";
+	    return false;
+	}
+
+	*val = ret;
+	return true;
+    }
+
+
     template<class Type>
     bool convert(char const * const optarg, Type* argp, std::string& error_str);
 
@@ -15,9 +138,20 @@ namespace libgetopt
 			std::string& error_str)
     {
 	assert(argp != NULL);
-	*argp = atoi(optarg);
-	return true;
+
+	return integral_convert<int>(optarg, error_str, argp);
     }
+
+
+    template<>
+    inline bool convert(char const * const optarg, long* argp,
+			std::string& error_str)
+    {
+	assert(argp != NULL);
+
+	return integral_convert<long>(optarg, error_str, argp);
+    }
+
 
     template<>
     inline bool convert(char const * const optarg, double* argp,
@@ -28,14 +162,6 @@ namespace libgetopt
 	return true;
     }
 
-    template<>
-    inline bool convert(char const * const optarg, long* argp,
-			std::string& error_str)
-    {
-	assert(argp != NULL);
-	*argp = atol(optarg);
-	return true;
-    }
 
     template<>
     inline bool convert(char const * const optarg, long long* argp,
--- src/test.cc
+++ src/test.cc
@@ -1,3 +1,4 @@
+#include <limits>
 #include <cstdio>
 #include <string>
 
@@ -5,20 +6,26 @@
 #include "cmdline_parser.h"
 
 using namespace libgetopt;
-using std::string;
+using namespace std;
 
 
 int main(int argc, char** argv)
 {
+    printf("int max: %d, int min: %d\n",
+	   numeric_limits<int>::max(),
+	   numeric_limits<int>::min());
+
     option<double> double_opt("double", 'd');
     option<int> int_opt("int", 'i');
     option<string> string_opt("string", 's');
+    option<long> long_opt("long", 'l');
 
     cmdline_parser parser;
 
     parser.add_option(&double_opt);
     parser.add_option(&int_opt);
     parser.add_option(&string_opt);
+    parser.add_option(&long_opt);
 
     cmdline_parser::parse_result res;
 
@@ -31,6 +38,12 @@ int main(int argc, char** argv)
 	if( res == cmdline_parser::parse_result::result_missing_arg )
 	{
 	    printf("option: %s, missing arg\n", res.error_option->long_option().c_str());
+	}
+	else if( res == cmdline_parser::parse_result::result_invalid_arg )
+	{
+	    printf("option: %s, invalid arg: %s\n",
+		   res.error_option->long_option().c_str(),
+		   res.error_string.c_str());
 	}
 
 	return 1;
@@ -49,6 +62,11 @@ int main(int argc, char** argv)
     if( string_opt.is_set() == true )
     {
 	printf("string option = %s\n", string_opt.get().c_str());
+    }
+
+    if( long_opt.is_set() == true )
+    {
+	printf("long option = %ld\n", long_opt.get());
     }
 
     return 0;
