4eb725a69b7e624361ad5ac6a39c00c38d48d9d1
nick <goodfella005@gmail.com>
2010-09-14 08:27:16 -0700

    Moved libgetopt header files to include directory:
    * This will make it easier to export the headers for future install.

--- include/libgetopt.h
+++ include/libgetopt.h
@@ -1,8 +1,8 @@
 #ifndef __LIBGETOPT_H__
 #define __LIBGETOPT_H__
 
-#include "../src/option.h"
-#include "../src/cmdline_parser.h"
-#include "../src/convert.h"
+#include "option.h"
+#include "cmdline_parser.h"
+#include "convert.h"
 
 #endif
