ab85c7c31ede541d5a4ff45e65286437122bc3a8
nick <goodfella005@gmail.com>
2010-09-14 22:36:29 -0700

    Rough cut of script that runs unit tests.

---
+++ unit-tests/run-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NUMERIC_TESTS_DIR="$(dirname $0)/numeric-tests"
+
+# numeric tests all run with no options
+for test in $(ls -1 "${NUMERIC_TESTS_DIR}")
+do
+    if [ -x $(dirname $0)/numeric-tests/$test ]; then
+
+	echo "running $test"
+	${NUMERIC_TESTS_DIR}/$test
+	TEST_STATUS="$?"
+
+	if [ "$TEST_STATUS" -ne "0" ]; then
+	    printf "%s FAILEDd with: %s\n" "$test" "$TEST_STATUS"
+	    exit 1
+	fi
+    fi
+done
+
+echo "All tests PASSED"
