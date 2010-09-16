3d69e6269dd51eded21deb29ed8c8a35c3c702a9
nick <goodfella005@gmail.com>
2010-09-15 22:59:09 -0700

    Run valgrind on numeric tests.

--- unit-tests/run-tests.sh
+++ unit-tests/run-tests.sh
@@ -1,11 +1,58 @@
 #!/bin/bash
 
+# 1 = binary path
+# 2 = log path
+function valgrind_test
+{
+    printf "\trunning valgrind on $1\n"
+    if ! valgrind --leak-check=full \
+	--show-reachable=yes \
+	--error-exitcode=1 \
+	--log-file="$2" \
+	${TEST_PATH}; then
+
+	printf "\t%s valgrind test FAILED, details are in %s\n" "$test" "$2"
+	return 1
+    else
+
+	if grep -q 'LEAK SUMMARY' "$2"; then
+
+	    printf "\t%s valgrind test FAILED details are in %s\n" "$test" "$2"
+	    return 1
+	fi
+    fi
+
+    rm -f "${VALGRIND_LOG}"
+    return 0
+}
+
 NUMERIC_TESTS_DIR="$(dirname $0)/numeric-tests"
+NUMERIC_TESTS="$(ls -1 ${NUMERIC_TESTS_DIR})"
+
+printf "Running numeric tests through valgrind:\n\n"
+
+for test in ${NUMERIC_TESTS}
+do
+    TEST_PATH="${NUMERIC_TESTS_DIR}/${test}"
+
+    if [ -x "${TEST_PATH}" ]; then
+
+	VALGRIND_LOG="${test}-valgrind.log"
+	if ! valgrind_test "${TEST_PATH}" "${VALGRIND_LOG}"; then
+	    exit 1
+	fi
+    fi
+
+done
+
+echo
+echo "valgrind tests passed"
+echo
 
 printf "Running numeric tests:\n\n"
 
 # numeric tests all run with no options
-for test in $(ls -1 "${NUMERIC_TESTS_DIR}")
+for test in ${NUMERIC_TESTS}
 do
     if [ -x $(dirname $0)/numeric-tests/$test ]; then
 
