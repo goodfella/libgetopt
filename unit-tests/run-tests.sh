b01b213f207f55ba660bce27a06dc47405de58da
nick <goodfella005@gmail.com>
2010-09-18 09:38:40 -0700

    Run valgrind tests after regular tests:
    * This insures that the tests actually passed before valgrind is run
      on them.  That way any errors detected by valgrind are actually
      memory related errors and not the error returned by the unit test.

--- unit-tests/run-tests.sh
+++ unit-tests/run-tests.sh
@@ -29,6 +29,25 @@ function valgrind_test
 NUMERIC_TESTS_DIR="$(dirname $0)/numeric-tests"
 NUMERIC_TESTS="$(ls -1 ${NUMERIC_TESTS_DIR})"
 
+printf "Running numeric tests:\n\n"
+
+# numeric tests all run with no options
+for test in ${NUMERIC_TESTS}
+do
+    if [ -x $(dirname $0)/numeric-tests/$test ]; then
+
+	printf "\trunning $test\n"
+	${NUMERIC_TESTS_DIR}/$test
+	TEST_STATUS="$?"
+
+	if [ "$TEST_STATUS" -ne "0" ]; then
+	    printf "\t%s FAILED with: %s\n" "$test" "$TEST_STATUS"
+	    exit 1
+	fi
+    fi
+done
+
+echo
 printf "Running numeric tests through valgrind:\n\n"
 
 for test in ${NUMERIC_TESTS}
@@ -49,23 +68,4 @@ echo
 echo "valgrind tests passed"
 echo
 
-printf "Running numeric tests:\n\n"
-
-# numeric tests all run with no options
-for test in ${NUMERIC_TESTS}
-do
-    if [ -x $(dirname $0)/numeric-tests/$test ]; then
-
-	printf "\trunning $test\n"
-	${NUMERIC_TESTS_DIR}/$test
-	TEST_STATUS="$?"
-
-	if [ "$TEST_STATUS" -ne "0" ]; then
-	    printf "\t%s FAILED with: %s\n" "$test" "$TEST_STATUS"
-	    exit 1
-	fi
-    fi
-done
-
-echo
 echo "All tests PASSED"
