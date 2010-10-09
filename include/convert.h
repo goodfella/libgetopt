9e9d97241ccc35007f24e90810474135a6ba6710
nick <goodfella005@gmail.com>
2010-10-08 21:46:16 -0700

    Redo of integral conversion logic.

--- include/convert.h
+++ include/convert.h
@@ -1,91 +1,18 @@
 #ifndef __CONVERT_H__
 #define __CONVERT_H__
 
-#include <errno.h>
-#include <cstdlib>
-#include <limits>
-#include <cassert>
 #include <string>
-
+#include <errno.h>
 
 namespace libgetopt
 {
-    template< class Type,
-	      class Func_Ret,
-	      class Func_Type,
-	      Func_Type Func,
-	      int Errno >
-    struct converter_info_base
-    {
-	    typedef Func_Ret func_return_t;
-	    static Func_Type func;
-	    static Type overflow_val;
-	    static Type underflow_val;
-	    static int range_errno;
-    };
-
-    template< class Type,
-	      class Func_Ret,
-	      class Func_Type,
-	      Func_Type Func,
-	      int Errno >
-    Func_Type converter_info_base<Type, Func_Ret, Func_Type, Func, Errno>::func = Func;
-
-    template< class Type,
-	      class Func_Ret,
-	      class Func_Type,
-	      Func_Type Func,
-	      int Errno >
-    Type converter_info_base<Type, Func_Ret, Func_Type, Func, Errno>::overflow_val =
-	std::numeric_limits<Type>::max();
-
-    template< class Type,
-	      class Func_Ret,
-	      class Func_Type,
-	      Func_Type Func,
-	      int Errno >
-    Type converter_info_base<Type, Func_Ret, Func_Type, Func, Errno>::underflow_val =
-	std::numeric_limits<Type>::min();
-
-
-    template< class Type,
-	      class Func_Ret,
-	      class Func_Type,
-	      Func_Type Func,
-	      int Errno >
-    int converter_info_base<Type, Func_Ret, Func_Type, Func, Errno>::range_errno = Errno;
-
-
-
-    template< class Type >
-    struct converter_info;
-
-    template<>
-    struct converter_info<int>: public converter_info_base<int,
-							   long,
-							   long (*)(char const * const, char**, int),
-							   &strtol,
-							   ERANGE>
-    {
-	    static int base;
-    };
-
-
-    template<>
-    struct converter_info<long>: public converter_info_base<long,
-							    long,
-							    long (*)(char const * const, char**, int),
-							    &strtol,
-							    ERANGE>
-
-    {
-	    static int base;
-    };
-
-
-    template<class Type>
-    bool integral_convert(char const * const num_str, std::string* err_str,
-			  Type* val)
+    template<class Ret, Ret (*Converter)(const char*, char**, int)>
+    bool integral_convert(char const * const num_str,
+			  std::string* err_str,
+			  Ret* val,
+			  Ret max,
+			  Ret min,
+			  int base)
     {
 	assert( val != NULL );
 
@@ -93,26 +20,19 @@ namespace libgetopt
 
 	errno = 0;
 
-	typename converter_info<Type>::func_return_t ret;
+	Ret ret = Converter(num_str, &bad_chrs, base);
 
-	ret = converter_info<Type>::func(num_str,
-					 &bad_chrs,
-					 converter_info<Type>::base);
-
-	if( errno == converter_info<Type>::range_errno )
+	if( errno == ERANGE )
 	{
-	    assert(ret >= converter_info<Type>::overflow_val ||
-		   ret <= converter_info<Type>::underflow_val );
+	    assert(ret >= max || ret <= min );
 
-	    if( ret >= converter_info<Type>::overflow_val )
+	    if( ret >= max )
 	    {
-		*err_str = "arg is too large";
-		return false;
+		goto overflow;
 	    }
-	    else if( ret <= converter_info<Type>::underflow_val )
+	    else if( ret <= min )
 	    {
-		*err_str = "arg is too small";
-		return false;
+		goto underflow;
 	    }
 	}
 	else if( errno != 0 || *bad_chrs != '\0' )
@@ -120,62 +40,46 @@ namespace libgetopt
 	    *err_str = "arg does not represent a numeric type";
 	    return false;
 	}
+	else if( ret > max )
+	{
+	    goto overflow;
+	}
+	else if( ret < min )
+	{
+	    goto underflow;
+	}
 
 	*val = ret;
 	return true;
+
+      overflow:
+
+	*err_str = "arg is too large";
+	return false;
+
+      underflow:
+
+	*err_str = "arg is too small";
+	return false;
+
     }
 
+    bool signed_integral_convert(char const * const num_str,
+				 std::string* err_str,
+				 long long int* val,
+				 long long int max,
+				 long long int min,
+				 int base);
+
+    bool unsigned_integral_convert(char const * const num_str,
+				   std::string* err_str,
+				   unsigned long long int* val,
+				   unsigned long long int max,
+				   unsigned long long int min,
+				   int base);
 
     template<class Type>
     bool convert(char const * const optarg, Type* argp, std::string* error_str);
-
-    template<>
-    inline bool convert(char const* const optarg, int* argp,
-			std::string* error_str)
-    {
-	assert(argp != NULL);
-
-	return integral_convert<int>(optarg, error_str, argp);
-    }
-
-
-    template<>
-    inline bool convert(char const * const optarg, long* argp,
-			std::string* error_str)
-    {
-	assert(argp != NULL);
-
-	return integral_convert<long>(optarg, error_str, argp);
-    }
-
-
-    template<>
-    inline bool convert(char const * const optarg, double* argp,
-			std::string* error_str)
-    {
-	assert(argp != NULL);
-	*argp = atof(optarg);
-	return true;
-    }
-
-
-    template<>
-    inline bool convert(char const * const optarg, long long* argp,
-			std::string* error_str)
-    {
-	assert(argp != NULL);
-	*argp = atoll(optarg);
-	return true;
-    }
-
-    template<>
-    inline bool convert(char const * const optarg, std::string* argp,
-			std::string* error_str)
-    {
-	assert(argp != NULL);
-	*argp = optarg;
-	return true;
-    }
 }
 
 #endif
--- src/convert.cc
+++ src/convert.cc
@@ -1,6 +1,111 @@
+#include <cstdlib>
+#include <limits>
+#include <cassert>
+#include <string>
+
 #include "convert.h"
 
-using namespace libgetopt;
+using std::numeric_limits;
+using std::string;
 
-int converter_info<int>::base = 10;
-int converter_info<long>::base = 10;
+namespace libgetopt
+{
+    bool signed_integral_convert(char const * const num_str,
+				 string* err_str,
+				 long long int* val,
+				 long long int max,
+				 long long int min, int base)
+    {
+	return integral_convert<long long int, strtoll>(num_str,
+							err_str,
+							val,
+							max,
+							min,
+							base);
+    }
+
+    bool unsigned_integral_convert(char const * const num_str,
+				   string* err_str,
+				   unsigned long long int* val,
+				   unsigned long long int max,
+				   unsigned long long int min,
+				   int base)
+    {
+	return integral_convert<unsigned long long int, strtoull>(num_str,
+								  err_str,
+								  val,
+								  max,
+								  min,
+								  base);
+    }
+
+    template<>
+    bool convert(char const* const optarg, int* argp,
+		 string* error_str)
+    {
+	assert(argp != NULL);
+
+	long long int val;
+
+	if( signed_integral_convert(optarg, error_str, &val,
+				    numeric_limits<int>::max(),
+				    numeric_limits<int>::min(),
+				    10) == true )
+	{
+	    *argp = static_cast<int>(val);
+	    return true;
+	}
+
+	return false;
+    }
+
+
+    template<>
+    bool convert(char const * const optarg, long* argp,
+		 string* error_str)
+    {
+	assert(argp != NULL);
+
+	long long int val;
+
+	if( signed_integral_convert(optarg, error_str, &val,
+				    numeric_limits<long>::max(),
+				    numeric_limits<long>::min(),
+				    10) == true )
+	{
+	    *argp = static_cast<long>(val);
+	    return true;
+	}
+
+	return false;
+    }
+
+
+    template<>
+    bool convert(char const * const optarg, double* argp,
+		 std::string* error_str)
+    {
+	assert(argp != NULL);
+	*argp = atof(optarg);
+	return true;
+    }
+
+
+    template<>
+    bool convert(char const * const optarg, long long* argp,
+		 std::string* error_str)
+    {
+	assert(argp != NULL);
+	*argp = atoll(optarg);
+	return true;
+    }
+
+    template<>
+    bool convert(char const * const optarg, std::string* argp,
+		 std::string* error_str)
+    {
+	assert(argp != NULL);
+	*argp = optarg;
+	return true;
+    }
+}
