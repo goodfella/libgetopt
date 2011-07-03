0a81d3fb4a4c472c06265cac8d3f0b53c8576ed3
nick <goodfella005@gmail.com>
2011-07-02 22:48:41 -0700

    Added a test for missing arguments.

---
+++ unit-tests/cmdline-parser-tests/option_missing_arg.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "libunit-test.h"
+
+using namespace std;
+using namespace libgetopt;
+using namespace libgetopt::unit_tests;
+
+int main(int argc, char** argv)
+{
+    option<int> opt("opt");
+    cmdline_parser parser;
+    cmdline_args args(argv[0]);
+
+    args.add(&opt);
+    parser.add_option(&opt);
+
+    parse_result res = parser.parse(args.count(), args);
+
+    if( res.result() != parse_result::result_missing_arg )
+    {
+	cerr << "expected missing arg\n";
+	return 1;
+    }
+
+    if( opt.name() != res.option_name() )
+    {
+	cerr << "incorrect option name: " << res.option_name() << endl;
+	return 1;
+    }
+
+    return 0;
+}
