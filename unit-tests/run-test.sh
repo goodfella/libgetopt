12a66611411ca2e4a7fbf140208c0c537dc56c7e
nick <goodfella005@gmail.com>
2011-07-26 23:20:04 -0700

    Changes to how unit tests are ran:
    * Actually run the test in run-test.sh.  By mistake, the test was not
      even ran because an uninitialized variable was being used to run the
      test instead of $1
    
    * In unit-tests/Makefile don't run valgrind on the unit test if the
      unit test fails.

--- unit-tests/run-test.sh
+++ unit-tests/run-test.sh
@@ -3,9 +3,12 @@
 # 1 = path to test
 
 echo "running ${1}"
-${test}
+${1}
 TEST_STATUS="$?"
 
 if [ "$TEST_STATUS" -ne "0" ]; then
     echo test failed and returned $TEST_STATUS
+    exit 1
 fi
+
+exit 0
