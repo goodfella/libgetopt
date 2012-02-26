850b25d9e0b4b95103191f0ca405c814842cc739
nick <goodfella005@gmail.com>
2012-02-26 08:36:32 -0800

    Split tests into two categories, unit-tests and integration tests

--- tests/test-macros.cmake
+++ tests/test-macros.cmake
@@ -1,8 +1,8 @@
 ENABLE_TESTING()
 
-MACRO(unit_test name source)
+MACRO(GETOPT_TEST name source)
   ADD_EXECUTABLE(${name} ${source})
   TARGET_LINK_LIBRARIES(${name} getopt unit-test)
   ADD_TEST(${name} ${name})
 #  ADD_TEST(${name}-valgrind ${PROJECT_BINARY_DIR}/valgrind-test.sh ${CMAKE_CURRENT_BINARY_DIR}/${name})
-ENDMACRO(unit_test)
+ENDMACRO(GETOPT_TEST)
